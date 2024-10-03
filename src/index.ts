af52b00c081a9e1213f2c2cd3ccfe3fba0a4ae34
Xalenty <98742670+Xalenty@users.noreply.github.com>
2024-10-03 15:59:53 +0200

    update redirection des URI

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ import { Hono } from 'hono'
 import { HomeController } from './controllers/HomeController';
 import { ReadAllCities } from './controllers/ReadAllCitiesController';
 import { serveStatic } from 'hono/bun'
+import { cities, parkings } from './data/staticDatabase';
+import { trimTrailingSlash } from 'hono/trailing-slash'
+
 
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
 
-const app = new Hono()
+const app = new Hono();
+app.use(trimTrailingSlash())
+
   .route('/', HomeController)
   .route('/cities', ReadAllCities)
 
