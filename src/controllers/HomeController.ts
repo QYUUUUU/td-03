09ddc311f05990fd1eb81ea548325896f1829122
Xalenty <98742670+Xalenty@users.noreply.github.com>
2024-10-03 15:30:34 +0200

    Css et update homepage

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -6,10 +6,11 @@ export const HomeController = new Hono()
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css"/>
       <title>Welcome to EuroPark</title>
     </head>
     <body>
-      <h1>Welcok</h1>
+      <h1>Welcome to EuroPark !</h1>
       <img src="./static/assets/images/parking.png" alt="EuroPark Parking">
       <p>
         Save time and money with EuroPark! Enjoy a 100% contactless parking
