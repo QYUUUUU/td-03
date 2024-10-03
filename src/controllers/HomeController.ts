f014d924c7c57067f826b8e78605191353422e1d
QYUUUUU <achoplin@gmail.com>
2024-10-03 12:38:57 +0000

    [FEATURE] Working controller and file structure static

---
+++ src/controllers/HomeController.ts
@@ -0,0 +1,25 @@
+import { Hono } from 'hono'
+
+export const HomeController = new Hono()
+  .get('/', (c) => c.html(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Welcome to EuroPark</title>
+    </head>
+    <body>
+      <h1>Welcok</h1>
+      <img src="/static/assets/images/parking.png" alt="EuroPark Parking">
+      <p>
+        Save time and money with EuroPark! Enjoy a 100% contactless parking
+        experience for a short or long duration in our car parks in Europe!
+      </p>
+      <nav>
+        <ul>
+          <li><a href="/cities">Our Cities</a></li>
+          <li><a href="/parkings">Our Car Parks</a></li>
+        </ul>
+      </nav>
+    </body>
+    </html>`));
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import { Hono } from 'hono'
+import { HomeController } from './controllers/HomeController';
+import { serveStatic } from 'hono/bun'
+
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
+
 const app = new Hono()
+  .route('/', HomeController)
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
+  app.use('/static/*', serveStatic({ root: './static' }))
 
 export default { 
-  port: process.env.PORT, 
+  port: process.env.PORT,
   fetch: app.fetch, 
 }
