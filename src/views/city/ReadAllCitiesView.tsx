bafb4cc517f37816685433a2647d792b87fb543b
Dugoua-Jacques Maé <152476556+MaeDj@users.noreply.github.com>
2024-10-04 11:54:09 +0200

    avancées etape2.3

---
+++ src/views/city/ReadAllCitiesView.tsx
@@ -0,0 +1,16 @@
+import {City} from  "../../models/classes/CityClass";
+import {Layout} from "./shared/Layout"
+//définition du type de paramètre
+type ReadAllCitiesViewProps = {
+cities: Array<City>;
+}
+//utilisation du composant Layout en tant que base
+//et définition du composant fonctionnel ReadAllCitiesView
+const ReadAllCitiesView =
+({ cities }: ReadAllCitiesViewProps) => 
+    <Layout>
+        
+        <div>Contenu à adapter</div>
+
+    </Layout>
+export default ReadAllCitiesView;
