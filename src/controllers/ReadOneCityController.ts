673c1483e89798721303b4a953f1450208a6a550
Dugoua-Jacques Maé <152476556+MaeDj@users.noreply.github.com>
2024-10-09 16:17:04 +0200

    View react uploade

--- src/controllers/ReadOneCityController.ts
+++ src/controllers/ReadOneCityController.ts
@@ -1,9 +1,12 @@
 import { Hono } from 'hono'
 import {ReadOneCityView} from "../views/city/ReadOneCityView"
 
+
 export const ReadOneCity = new Hono()
 
-ReadOneCity.get('/', (c) => {
-    const html = ReadOneCityView();
+ReadOneCity.get('/', (c) =>{
+
+    var url1="aa"
+    const html = ReadOneCityView(url1);
     return c.html(html);
   });
--- src/views/city/ReadOneCityView.tsx
+++ src/views/city/ReadOneCityView.tsx
@@ -1,12 +1,25 @@
 import {Layout} from "./shared/Layout"
-
-export const ReadOneCityView =
-() =>{
+import { cities } from '../../data/staticDatabase';
+export const ReadOneCityView=
+(url:String) =>{
+    // const getCityIndex =():number=>{
+    //     const loc=useLocation()
+    //     var returnIndex:number=-1
+    //     for(let i=0;i<cities.length;i++){
+    //         if(cities[i].slug===loc.pathname){
+    //             returnIndex=i
+    //         }
+    //     }
+    //     return(returnIndex)
+    // }
+    // var actualCityIndex:number=getCityIndex()
+    
     return(
-    <Layout pageTitle="one city">
+    <Layout pageTitle="Your city">
         <div>
-            you are here 
+            <p> L'url est {url}</p>
         </div>
+        
     </Layout>
     )
 }    
